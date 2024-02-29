eeb17c0445a9ca8cfdd0f87f2e89925ea082b7ac
Gamalsobhy585 <eng.gamalsobhi@gmail.com>
2024-02-29 23:57:33 +0200

    day1tasksuploaded

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Task1 from './task1';
+import Task2 from './task2';
+
+function App() {
+  return (
+    <div className="App">
+      <Task1 />
+      <hr></hr>
+      <Task2 />
+   
+    </div>
+  );
+}
+
+export default App;
